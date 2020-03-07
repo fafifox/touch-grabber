8a6a435ab3ebca8593560e4936b8d724b4b14d84
farooch <hugomalatrait@gmail.com>
2020-03-07 12:48:42 +0100

    Added path variable & Cleaned useless comments

--- cmd/touch-grabber/main.go
+++ cmd/touch-grabber/main.go
@@ -9,17 +9,8 @@ import (
 func main() {
 	config := versions.GetConfig()
 	buildVersion := versions.GetBuildVersion()
-	//appVersion := versions.GetApplication().Results[0].Version
-	//assetsVersion := versions.GetAssets().AssetsVersion
-	//staticDataVersion := versions.GetAssets().StaticDataVersion
-	//assetsUrl := config.AssetsURL
 	dataUrl := config.DataURL
-	//fmt.Println("Build Version: ", buildVersion)
-	//fmt.Println("Application Version: ", appVersion)
-	//fmt.Println("Assets Version: ", assetsVersion)
-	//fmt.Println("Static Data Version: ", staticDataVersion)
-	//fmt.Println("Assets URL: ", assetsUrl)
 	fmt.Println("Data URL: ", dataUrl)
-	//data.FetchDataMap(dataUrl, "fr", buildVersion, "Jobs")
-	data.FetchAllDataMap(dataUrl, "fr", buildVersion)
+	//data.FetchDataMap(dataUrl, "fr", buildVersion, "./", "Jobs")
+	data.FetchAllDataMap(dataUrl, "fr", buildVersion, "./")
 }
--- pkg/data/map.go
+++ pkg/data/map.go
@@ -9,38 +9,21 @@ import (
 	"os"
 )
 
-//https://proxyconnection.touch.dofus.com/data/map?lang=fr&v=1.45.4
-
-
-//Invoke-WebRequest -Uri "https://proxyconnection.touch.dofus.com/data/map?lang=fr&v=1.45.4"
-//-Method "POST"
-//-Headers @{
-//	"Origin"="file://";
-//	"Accept-Encoding"="gzip, deflate, br";
-//	"Accept-Language"="fr";
-//	"User-Agent"="Mozilla/5.0 (Linux; Android 5.1.1; SHIELD Tablet Build/LMY48C; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.113 Mobile Safari/537.36";
-//	"Accept"="*/*";
-//	"Cookie"="io=f8DC6oCt7ST3FEA6BTAG"
-//	}
-//-ContentType "application/json"
-//-Body "{`"class`":`"TYPE`"}"
-//-outfile "TYPE.json"
-
 type dataMapData struct {
-	Data      []byte
-	Name      string
+	Data []byte
+	Name string
 }
 
-func FetchAllDataMap(dataUrl string, langage string, buildVersion string) {
+func FetchAllDataMap(dataUrl string, langage string, buildVersion string, path string) {
 	for _, typeName := range typeNames {
-		FetchDataMap(dataUrl, langage, buildVersion, typeName)
+		FetchDataMap(dataUrl, langage, buildVersion, path, typeName)
 	}
 }
 
-func FetchDataMap(dataUrl string, langage string, buildVersion string, typeName string) {
+func FetchDataMap(dataUrl string, langage string, buildVersion string, path string, typeName string) {
 	url := dataUrl + "/data/map?lang=" + langage + "&v=" + buildVersion
 	requestBody, err := json.Marshal(map[string]string{
-		"class" : typeName,
+		"class": typeName,
 	})
 	if err != nil {
 		log.Println(err)
@@ -58,15 +41,16 @@ func FetchDataMap(dataUrl string, langage string, buildVersion string, typeName 
 		Data: body,
 		Name: typeName + ".json",
 	}
-	err = SaveDataMap(&dataMapData)
+	err = SaveDataMap(&dataMapData, buildVersion, path)
 	if err != nil {
 		log.Println(err)
 	}
 	log.Println("Saved: ", dataMapData.Name)
 }
 
-func SaveDataMap(data *dataMapData) error {
-	path := "output"
+func SaveDataMap(data *dataMapData, buildVersion string, path string) error {
+	folder := "/dataMap-v" + buildVersion
+	path = path + folder
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err = os.Mkdir(path, os.ModePerm)
 	}
